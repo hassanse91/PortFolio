8d2aa7debac254e02e417a7664e4dbb8ab596dec
hassanse91 <senhajihassan10@gmail.com>
2016-08-03 12:57:37 +0000

    commit

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module LiveMentor
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    config.assets.compile = true
   end
 end
